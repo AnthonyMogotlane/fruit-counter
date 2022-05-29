f2beb45ea63bea400e14ab3b1b14622b6f8e079e
Anthony Mogotlane <almogotlane@gmail.com>
2022-05-29 17:39:36 +0200

    Added extra tests

--- test/fruit-counter.test.js
+++ test/fruit-counter.test.js
@@ -9,7 +9,17 @@ describe("The FruitCounter Function", () => {
         assert.equal(fruitCounter.count(), 1);
     })
 
-    it("should be able to increase the counter 4 time", () => {
+    it("should be able to initialize the counter to a specific value and increase the counter/increment", () => {
+        let fruitCounter = FruitCounter(7);
+        
+        assert.equal(fruitCounter.count(), 7);
+        
+        fruitCounter.increase();
+        
+        assert.equal(fruitCounter.count(), 8);
+    })
+
+    it("should be able to increase the counter 4 times", () => {
         let fruitCounter = FruitCounter();
         
         assert.equal(fruitCounter.count(), 0);
@@ -39,6 +49,19 @@ describe("The FruitCounter Function", () => {
         assert.equal(fruitCounter.count(), 1);
     })
 
+
+    it("should be able to decrease the counter from the initiated counter value", () => {
+        let fruitCounter = FruitCounter(7);
+        
+        assert.equal(fruitCounter.count(), 7);
+        
+        fruitCounter.decrease();
+        
+        assert.equal(fruitCounter.count(), 6);
+    })
+
+
+
     it("should not be able to decrease the counter if the counter is 0", () => {
         let fruitCounter = FruitCounter();
         
@@ -61,5 +84,32 @@ describe("The FruitCounter Function", () => {
         fruitCounter.decrease();
         
         assert.equal(fruitCounter.count(), 0);
+    })
+
+    it("should be able to count apples, oranges and pears independently from each other", () => {
+        let appleCounter = FruitCounter();
+        let orangeCounter = FruitCounter();
+        let pearCounter = FruitCounter();
+        
+        appleCounter.increase();
+        appleCounter.increase();
+        appleCounter.increase();
+
+        orangeCounter.increase();
+        orangeCounter.increase();
+        orangeCounter.increase();
+        orangeCounter.increase();       
+        
+        pearCounter.increase();
+        pearCounter.increase();
+        pearCounter.increase();
+        pearCounter.increase();        
+        pearCounter.increase();        
+
+      
+        assert.equal(appleCounter.count(), 3);
+        assert.equal(orangeCounter.count(), 4);
+        assert.equal(pearCounter.count(), 5);
+
     })
 })
