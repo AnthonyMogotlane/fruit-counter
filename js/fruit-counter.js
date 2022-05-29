b7ec22ef2817f336441dfd435dd4e71e653535d8
Anthony Mogotlane <almogotlane@gmail.com>
2022-05-29 13:55:16 +0200

    Added logic to increase and decrease the counter

---
+++ js/fruit-counter.js
@@ -0,0 +1,22 @@
+const FruitCounter = () => {
+    let theCount = 0;
+
+    //function to increment the counter
+    const increase = () => {
+        theCount ++;
+    }
+
+    //function to increment the counter
+    const decrease = () => {
+        if (theCount > 0) theCount --;
+    }
+    //get the counter
+    //function to increment the counter
+    const count = () => theCount;
+
+    return {
+        increase,
+        decrease,
+        count
+    }
+ }
